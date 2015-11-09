d93ef2c2ec52a1198323729e167abbaea9888fea
Isidro Lopez <islomar@gmail.com>
2015-11-09 16:41:56 +0100

    Recognise Should suffix as test class.

--- src/test/java/com/islomar/yaus/web/UrlShortenerApplicationShould.java
+++ src/test/java/com/islomar/yaus/web/UrlShortenerApplicationShould.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = UrlShortenerApplication.class)
 @WebAppConfiguration
-public class UrlShortenerApplicationTests {
+public class UrlShortenerApplicationShould {
 
 	@Test
 	public void contextLoads() {
