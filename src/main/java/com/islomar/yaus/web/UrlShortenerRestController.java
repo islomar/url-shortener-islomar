140634ba74fd3ba4fedbdcfc26f889a8aa9161f8
Isidro Lopez <islomar@gmail.com>
2015-11-12 10:36:54 +0100

    Use logback for logging

--- src/main/java/com/islomar/yaus/web/UrlShortenerRestController.java
+++ src/main/java/com/islomar/yaus/web/UrlShortenerRestController.java
@@ -2,6 +2,8 @@ package com.islomar.yaus.web;
 
 import com.islomar.yaus.core.URLShortenerService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -25,6 +27,8 @@ import javax.servlet.http.HttpServletResponse;
 @RestController
 public class UrlShortenerRestController {
 
+  private final static Logger LOG = LoggerFactory.getLogger(UrlShortenerRestController.class);
+
   private final URLShortenerService urlShortenerService;
 
   @Autowired
@@ -42,6 +46,7 @@ public class UrlShortenerRestController {
   @RequestMapping(value = "/{shortUrlId}", method = RequestMethod.GET)
   void redirectToFullUrl(@PathVariable String shortUrlId, HttpServletResponse response) throws IOException {
 
+    LOG.debug("Received request to redirect to '%s'", shortUrlId);
     Optional<URL> shortenedURLFound = urlShortenerService.findURLById(shortUrlId);
     if (shortenedURLFound.isPresent()) {
       response.sendRedirect(shortenedURLFound.get().toString());
@@ -53,11 +58,13 @@ public class UrlShortenerRestController {
   @RequestMapping(value = "/", method = RequestMethod.POST, consumes = MediaType.TEXT_PLAIN_VALUE)
   ResponseEntity<String> createShortUrl(@RequestBody String uriStringToBeShortened, HttpServletRequest request) {
 
+    LOG.debug("Received request to shorten the URL '%s'", uriStringToBeShortened);
     try {
       String shortenedUrlId = urlShortenerService.shorten(uriStringToBeShortened);
       String shortenedUrl = request.getRequestURL() + shortenedUrlId;
       return new ResponseEntity<String>(shortenedUrl, HttpStatus.CREATED);
     } catch (IllegalArgumentException | MalformedURLException ex) {
+      LOG.error("Error when trying to create a shortened URL for %s: %s", uriStringToBeShortened, ex.getMessage());
       return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
     }
 
