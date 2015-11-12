b1e3cae159c446aedb1fd3348bbf9a2972ea6fff
Isidro Lopez <islomar@gmail.com>
2015-11-12 12:53:42 +0100

    Create test for InMemoryShortenedUrlRepository

--- src/test/java/com/islomar/yaus/core/infrastructure/repositories/InMemoryShortenedUrlRepositoryShould.java
+++ src/test/java/com/islomar/yaus/core/infrastructure/repositories/InMemoryShortenedUrlRepositoryShould.java
@@ -1,8 +1,39 @@
 package com.islomar.yaus.core.infrastructure.repositories;
 
-/**
- *
- */
+import org.junit.Before;
+import org.junit.Test;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+
+import static java.lang.Boolean.FALSE;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class InMemoryShortenedUrlRepositoryShould {
+
+  private static final URI OSOCO_URI = URI.create("http://www.osoco.es");
+  public static final String OSOCO_URL_MURMUR3_HASH = "339d3b53";
+
+  private InMemoryShortenedUrlRepository inMemoryShortenedUrlRepository;
+
+  @Before
+  public void setUp() {
+    this.inMemoryShortenedUrlRepository = new InMemoryShortenedUrlRepository();
+  }
+
+  @Test public void
+  store_a_shortened_url() throws MalformedURLException {
+
+    inMemoryShortenedUrlRepository.save(OSOCO_URL_MURMUR3_HASH, OSOCO_URI.toURL());
+
+    assertThat(inMemoryShortenedUrlRepository.findByShortenedURI(OSOCO_URL_MURMUR3_HASH).get(), is(OSOCO_URI.toURL()));
+  }
+
+  @Test public void
+  return_absent_optional_for_a_non_existing_shortened_url() throws MalformedURLException {
+
+    assertThat(inMemoryShortenedUrlRepository.findByShortenedURI(OSOCO_URL_MURMUR3_HASH).isPresent(), is(FALSE));
+  }
 
 }
--- src/test/java/com/islomar/yaus/core/infrastructure/services/Murmur3With32BitsHashGeneratorShould.java
+++ src/test/java/com/islomar/yaus/core/infrastructure/services/Murmur3With32BitsHashGeneratorShould.java
@@ -15,8 +15,7 @@ public class Murmur3With32BitsHashGeneratorShould {
     this.murmur3With32BitsHashGenerator = new Murmur3With32BitsHashGenerator();
   }
 
-  @Test
-  public void
+  @Test public void
   generate_339d3b53_for_osoco_url() {
     assertThat(murmur3With32BitsHashGenerator.generateIdFrom("http://www.osoco.es"), is("339d3b53"));
   }
