c25fef5cfc8639bbe9cf776b0f5e50f763f3bd37
Isidro Lopez <islomar@gmail.com>
2015-11-12 12:13:59 +0100

    Use logback instead of System.out.println in UrlShortenerApplication

--- src/main/java/com/islomar/yaus/UrlShortenerApplication.java
+++ src/main/java/com/islomar/yaus/UrlShortenerApplication.java
@@ -1,5 +1,7 @@
 package com.islomar.yaus;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -9,15 +11,17 @@ import java.util.Arrays;
 @SpringBootApplication
 public class UrlShortenerApplication {
 
+  private final static Logger LOG = LoggerFactory.getLogger(UrlShortenerApplication.class);
+
   public static void main(String[] args) {
     ApplicationContext ctx = SpringApplication.run(UrlShortenerApplication.class, args);
 
-    System.out.println("Let's inspect the beans provided by Spring Boot:");
+    LOG.debug("Let's inspect the beans provided by Spring Boot:");
 
     String[] beanNames = ctx.getBeanDefinitionNames();
     Arrays.sort(beanNames);
     for (String beanName : beanNames) {
-      System.out.println(beanName);
+      LOG.debug(beanName);
     }
   }
 }
