e7fd79fc78386a7cdf08ac79580e83050cb4ac96
Isidro Lopez <islomar@gmail.com>
2015-11-12 10:24:48 +0100

    Create test for Murmur3With32BitsHashGenerator

--- src/test/java/com/islomar/yaus/core/Murmur3With32BitsHashGeneratorShould.java
+++ src/test/java/com/islomar/yaus/core/Murmur3With32BitsHashGeneratorShould.java
@@ -1,8 +1,23 @@
 package com.islomar.yaus.core;
 
-/**
- *
- */
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class Murmur3With32BitsHashGeneratorShould {
 
+  private Murmur3With32BitsHashGenerator murmur3With32BitsHashGenerator;
+
+  @Before
+  public void setUp() {
+    this.murmur3With32BitsHashGenerator = new Murmur3With32BitsHashGenerator();
+  }
+
+  @Test
+  public void
+  generate_339d3b53_for_osoco_url() {
+    assertThat(murmur3With32BitsHashGenerator.generateIdFrom("http://www.osoco.es"), is("339d3b53"));
+  }
 }
